ecb6b670a2129ff6aaf897a9006d738640b10deb
logx1 <115801200+logx1@users.noreply.github.com>
2022-12-28 01:46:46 +0100

    Add files via upload

---
+++ draw.c
@@ -0,0 +1,54 @@
+#include "test.h"
+float max(float x, float y)
+{
+    if (x > y)
+    {
+        return (x);
+    }
+    return (y);
+}
+float abss(float x)
+{
+    if (x >= 0)
+    {
+        return (x);
+    }
+    return (-x);
+}
+void draw(void *mlx_ptr, void *win_ptr, float x0,float y0,float x1,float y1)
+{
+    float x_styp;
+    float y_styp;
+
+    x_styp = x1 - x0;
+    y_styp = y1 - y0;
+
+    x_styp /= max(abss(x_styp),abss(y_styp));
+    y_styp /= max(abss(x_styp),abss(y_styp));
+    while ((int)(x0 - x1) || (int)(y0 - y1))
+    {
+        mlx_pixel_put(mlx_ptr, win_ptr,x0,y0, 0x75d36e);
+        x0 += x_styp;
+        y0 += y_styp;
+    }
+    
+}
+
+
+int main(void)
+{
+    void *mlx_ptr;
+    void *win_ptr;
+   
+    
+    mlx_ptr =mlx_init();
+     win_ptr = mlx_new_window(mlx_ptr, 500, 500 , "test");
+
+   draw(mlx_ptr,win_ptr,50,50,40,400);
+        
+    
+    
+     mlx_loop(mlx_ptr);
+    return (0);
+       
+}
